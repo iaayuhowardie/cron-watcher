6ca837f5d09b353b519c18caa97dcfabce5dd5f3
Aayush Howard <iaayuhowardie@users.noreply.github.com>
2026-05-15 17:03:25 +0800

    feat(notifiers): add get_notifier factory function

--- cron_watcher/notifiers/__init__.py
+++ cron_watcher/notifiers/__init__.py
@@ -3,3 +3,30 @@ from cron_watcher.notifiers.slack_notifier import SlackNotifier
 from cron_watcher.notifiers.webhook_notifier import WebhookNotifier
 
 __all__ = ["EmailNotifier", "SlackNotifier", "WebhookNotifier"]
+
+
+def get_notifier(notifier_type: str, **kwargs):
+    """Factory function to get a notifier instance by type name.
+
+    Args:
+        notifier_type: The type of notifier to create. One of 'email',
+            'slack', or 'webhook'.
+        **kwargs: Keyword arguments passed to the notifier's constructor.
+
+    Returns:
+        An instance of the requested notifier.
+
+    Raises:
+        ValueError: If the notifier_type is not recognized.
+    """
+    notifiers = {
+        "email": EmailNotifier,
+        "slack": SlackNotifier,
+        "webhook": WebhookNotifier,
+    }
+    if notifier_type not in notifiers:
+        raise ValueError(
+            f"Unknown notifier type '{notifier_type}'. "
+            f"Valid options are: {', '.join(notifiers.keys())}"
+        )
+    return notifiers[notifier_type](**kwargs)
